086d0a6d525f0544f47b2c0d736bab0312a0f755
T- <T-@PC191>
2017-05-26 21:44:53 -0500

    controladora y json

--- src/main/java/org/rocko/bimestral/ControladorHola.java
+++ src/main/java/org/rocko/bimestral/ControladorHola.java
@@ -5,13 +5,37 @@
  */
 package org.rocko.bimestral;
 
+//crossOrigin para aplicaciones distribuidas (n-ramas)
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
+@CrossOrigin
 public class ControladorHola {
 
+    //1er parametro mapeo, segundo es el metodo, tercer parametro headers(formato de respuesta)
+@RequestMapping(value="/hola/{mensaje}",method=RequestMethod.GET,headers={"Accept=application/vnd.ms-excel"})
+public String saludar(@PathVariable String mensaje){
+    String algo="bienvenido " + mensaje;
+    return algo;
+}
+@RequestMapping(value="/usuario",method=RequestMethod.GET,headers={"Accept=application/json"})
+    public ArrayList<Usuario> obtener(){
+        
+        Usuario u1= new Usuario("Rocko","r",26);
+        Usuario u2= new Usuario("Leonidas","Leo",2);
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        
+        usuarios.add (u1);
+        usuarios.add(u2);
+        return usuarios;
+    }
 
-    
+
 }
